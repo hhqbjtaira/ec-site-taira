0762e0bdc763188c0bda411563941eab5726793e
Yokoyama-kouki <0k7u1k8@gmail.com>
2021-11-05 04:13:13 +0900

    Validation書き方修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,21 +1,21 @@
 class User < ApplicationRecord
   VALID_PASSWORD_REGEX = /\A[a-zA-Z0-9-]+\z/
+  VALID_ZIPCODE_REGEX = /\A\d{3}[-]\d{4}\z/
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  VALID_PHONE_NUMBER_REGEX = /\A[0-9]+\z/
   belongs_to :user_classification
 
   with_options presence: true do
-    VALID_PASSWORD_REGEX = /\A[a-z0-9]+\z/i
     validates :password, length: { in: 6..15 }, format: { with: VALID_PASSWORD_REGEX }
     validates :last_name, length: { maximum: 10 }
     validates :first_name, length: { maximum: 10 }
-    VALID_ZIPCODE_REGEX = /\A\d{3}[-]\d{4}\z/
     validates :zipcode, format: { with: VALID_ZIPCODE_REGEX }
     validates :prefecture, length: { maximum: 5 }
     validates :municipality, length: { maximum: 10 }
     validates :address, length: { maximum: 15 }
     validates :apartments, length: { maximum: 20 }
-    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
     validates :email, format: { with: VALID_EMAIL_REGEX }, uniqueness: true
-    validates :phone_number, length: { maximum: 15 }, format: { with: /\A[0-9]+\z/ }
+    validates :phone_number, length: { maximum: 15 }, format: { with: VALID_PHONE_NUMBER_REGEX }
   end
 
 end
