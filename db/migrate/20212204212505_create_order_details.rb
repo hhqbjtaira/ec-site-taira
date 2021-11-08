99b19919197778cf9b9e776932bd3043e6c083ed
Yokoyama-kouki <0k7u1k8@gmail.com>
2021-11-08 16:40:02 +0900

    Migrationファイル修正

--- db/migrate/20212204212505_create_order_details.rb
+++ db/migrate/20212204212505_create_order_details.rb
@@ -1,9 +1,9 @@
 class CreateOrderDetails < ActiveRecord::Migration[6.0]
   def change
     create_table :order_details do |t|
-      t.references :products_id, null: false, foreign_key: true
-      t.references :order_id, null: false, foreign_key: true
-      t.references :shipment_status_id, null: false, foreign_key: true
+      t.references :products, null: false, foreign_key: true
+      t.references :order, null: false, foreign_key: true
+      t.references :shipment_status, null: false, foreign_key: true
       t.string :order_detail_number, :limit => 64
       t.integer :order_quantity
       t.datetime :shipment_date
