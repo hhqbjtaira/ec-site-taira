1ed25a5f33e3a4a0916de16c8d0fa1d3e5e5c2b4
Taira <89443713+hhqbjtaira@users.noreply.github.com>
2021-11-08 22:51:36 +0900

    Product関連のseedの書き方の修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,67 +16,64 @@ UserClassification.create!(user_classification_name: "general")
 end
 
 Category.create!(category_name: "家具")
-2.times do |i|
-  Product.create!(product_name: "テーブル",
-                 category_id: 1,
-                 price: 10000,
-                 description: "商品説明",
-                 sale_status_id: 1,
-                 product_status_id: 1,
-                 regist_date:DateTime.parse('1993-02-24T12:30:45'),
-                 user_id: 1,
-                 delete_flag: false)
-end
+Category.create!(category_name: "家具")
 
 SaleStatus.create!(sale_status_name: "準備中")
-2.times do |i|
-  Product.create!(product_name: "テーブル",
-                 category_id: 1,
-                 price: 10000,
-                 description: "商品説明",
-                 sale_status_id: 1,
-                 product_status_id: 1,
-                 regist_date:DateTime.parse('1993-02-24T12:30:45'),
-                 user_id: 1,
-                 delete_flag: false)
-end
+SaleStatus.create!(sale_status_name: "準備中")
 
-ProductStatuse.create!(product_status_name: "新品")
-2.times do |i|
-  Product.create!(product_name: "テーブル",
-                 category_id: 1,
-                 price: 10000,
-                 description: "商品説明",
-                 sale_status_id: 1,
-                 product_status_id: 1,
-                 regist_date:DateTime.parse('1993-02-24T12:30:45'),
-                 user_id: 1,
-                 delete_flag: false)
-end
+ProductStatus.create!(product_status_name: "新品")
+ProductStatus.create!(product_status_name: "新品")
 
 Product.create!(product_name: "テーブル",
                category_id: 1,
-               price: 1000,
+               price: 10000,
                description: "商品説明",
                sale_status_id: 1,
                product_status_id: 1,
                regist_date:DateTime.parse('1993-02-24T12:30:45'),
                user_id: 1,
                delete_flag: false)
-2.times do |i|
-  Purchas.create!(purchase_price: 10000,
-                 purchase_quantity: 1,
-                 purchase_company: "株式会社aaa",
-                 order_date:DateTime.parse('1993-02-24T12:30:45'),
-                 purchase_date:DateTime.parse('1993-02-24T12:30:45'),
-                 product_id: 1)
-end
 
-Purchas.create!(purchase_price: 10000,
-               purchase_quantity: 1,
-               purchase_company: "株式会社aaa",
-               order_date:DateTime.parse('1993-02-24T12:30:45'),
-               purchase_date:DateTime.parse('1993-02-24T12:30:45'),
-               product_id: 1)
-2.times do |i|
-end
+Product.create!(product_name: "テーブル",
+               category_id: 1,
+               price: 10000,
+               description: "商品説明",
+               sale_status_id: 1,
+               product_status_id: 1,
+               regist_date:DateTime.parse('1993-02-24T12:30:45'),
+               user_id: 1,
+               delete_flag: false)
+
+Product.create!(product_name: "テーブル",
+               category_id: 1,
+               price: 10000,
+               description: "商品説明",
+               sale_status_id: 1,
+               product_status_id: 1,
+               regist_date:DateTime.parse('1993-02-24T12:30:45'),
+               user_id: 1,
+               delete_flag: false)
+
+Product.create!(product_name: "テーブル",
+               category_id: 1,
+               price: 10000,
+               description: "商品説明",
+               sale_status_id: 1,
+               product_status_id: 1,
+               regist_date:DateTime.parse('1993-02-24T12:30:45'),
+               user_id: 1,
+               delete_flag: false)
+
+Purchase.create!(purchase_price: 10000,
+                purchase_quantity: 1,
+                purchase_company: "株式会社aaa",
+                order_date:DateTime.parse('1993-02-24T12:30:45'),
+                purchase_date:DateTime.parse('1993-02-24T12:30:45'),
+                product_id: 1)
+
+Purchase.create!(purchase_price: 10000,
+                purchase_quantity: 1,
+                purchase_company: "株式会社aaa",
+                order_date:DateTime.parse('1993-02-24T12:30:45'),
+                purchase_date:DateTime.parse('1993-02-24T12:30:45'),
+                product_id: 1)
