3ca7f567cbee1ac909079d9ac79d198193c832a4
Taira <89443713+hhqbjtaira@users.noreply.github.com>
2021-11-08 19:14:36 +0900

    seeds.rb修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,20 @@
+UserClassification.create!(user_classification_name: "admin")
+UserClassification.create!(user_classification_name: "general")
+2.times do |i|
+  User.create!(password: "qwerty",
+              last_name: "user#{i}",
+              first_name: "user#{i}",
+              zipcode: "731-319#{i}",
+              prefecture: "広島県",
+              municipality: "広島市安佐南区",
+              address: "大塚東",
+              apartments: "1-1-#{i}",
+              email: "a#{i}@example.com",
+              phone_number: "082123000#{i}",
+              user_classification_id: 1,
+              company_name: "株式会社aaa")
+end
+
 Category.create!(category_name: "家具")
 2.times do |i|
   Product.create!(product_name: "テーブル",
@@ -55,19 +72,11 @@ Product.create!(product_name: "テーブル",
                  product_id: 1)
 end
 
-UserClassification.create!(user_classification_name: "admin")
-UserClassification.create!(user_classification_name: "general")
+Purchas.create!(purchase_price: 10000,
+               purchase_quantity: 1,
+               purchase_company: "株式会社aaa",
+               order_date:DateTime.parse('1993-02-24T12:30:45'),
+               purchase_date:DateTime.parse('1993-02-24T12:30:45'),
+               product_id: 1)
 2.times do |i|
-  User.create!(password: "qwerty",
-              last_name: "user#{i}",
-              first_name: "user#{i}",
-              zipcode: "731-319#{i}",
-              prefecture: "広島県",
-              municipality: "広島市安佐南区",
-              address: "大塚東",
-              apartments: "1-1-#{i}",
-              email: "a#{i}@example.com",
-              phone_number: "082123000#{i}",
-              user_classification_id: 1,
-              company_name: "株式会社aaa")
 end
