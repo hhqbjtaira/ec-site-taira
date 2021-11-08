4a4f4d1b645fe24fa564e3b1c75dc61f82616718
Taira <89443713+hhqbjtaira@users.noreply.github.com>
2021-11-08 12:34:33 +0900

    データ投入 Seed Products関連

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,65 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Category.create!(category_name: '家具')
+2.times do |i|
+  Product.create!(Product.create!(
+    product_name: 'テーブル',
+    category_id: 1,
+    price: 10000,
+    description: '商品説明',
+    sale_status_id: 1,
+    product_status_id: 1,
+    regist_date:DateTime.parse('1993-02-24T12:30:45'),
+    user_id: 1,
+    delete_flag: false)
+  end
+end
+
+SaleStatus.create!(sale_status_name: '準備中')
+2.times do |i|
+  Product.create!(Product.create!(
+    product_name: 'テーブル',
+    category_id: 1,
+    price: 10000,
+    description: '商品説明',
+    sale_status_id: 1,
+    product_status_id: 1,
+    regist_date:DateTime.parse('1993-02-24T12:30:45'),
+    user_id: 1,
+    delete_flag: false)
+  end
+end
+
+ProductStatuse.create!(product_status_name: '新品')
+2.times do |i|
+  Product.create!(Product.create!(
+    product_name: 'テーブル',
+    category_id: 1,
+    price: 10000,
+    description: '商品説明',
+    sale_status_id: 1,
+    product_status_id: 1,
+    regist_date:DateTime.parse('1993-02-24T12:30:45'),
+    user_id: 1,
+    delete_flag: false)
+  end
+end
+
+Product.create!(
+  product_name: 'テーブル',
+  category_id: 1,
+  price: 1000,
+  description: '商品説明',
+  sale_status_id: 1,
+  product_status_id: 1,
+  regist_date:DateTime.parse('1993-02-24T12:30:45'),
+  user_id: 1,
+  delete_flag: )
+2.times do |i|
+  Purchas.create!(
+    purchase_price: 10000,
+    purchase_quantity: 1,
+    purchase_company: '株式会社aaa',
+    order_date:DateTime.parse('1993-02-24T12:30:45'),
+    purchase_date:DateTime.parse('1993-02-24T12:30:45'),
+    product_id: 1)
+  end
+end
