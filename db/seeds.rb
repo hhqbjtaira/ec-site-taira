82ca115a33890dea24e328b54fcc28e247c2b37a
Taira <89443713+hhqbjtaira@users.noreply.github.com>
2021-11-09 17:17:35 +0900

    Seed.rbの修正（商品名、商品説明、id など)

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,64 +16,64 @@ UserClassification.create!(user_classification_name: "general")
 end
 
 Category.create!(category_name: "家具")
-Category.create!(category_name: "家具")
+Category.create!(category_name: "文房具")
 
-SaleStatus.create!(sale_status_name: "準備中")
-SaleStatus.create!(sale_status_name: "準備中")
+SaleStatus.create!(sale_status_name: "販売中")
+SaleStatus.create!(sale_status_name: "売り切れ")
 
 ProductStatus.create!(product_status_name: "新品")
-ProductStatus.create!(product_status_name: "新品")
+ProductStatus.create!(product_status_name: "新品同様")
 
 Product.create!(product_name: "テーブル",
                category_id: 1,
                price: 10000,
-               description: "商品説明",
+               description: "商品説明:木目を活かした、和室でも、洋室でも使えるナチュラルなローテーブルです",
                sale_status_id: 1,
                product_status_id: 1,
                regist_date:DateTime.parse('1993-02-24T12:30:45'),
                user_id: 1,
                delete_flag: false)
 
-Product.create!(product_name: "テーブル",
+Product.create!(product_name: "椅子",
                category_id: 1,
-               price: 10000,
-               description: "商品説明",
+               price: 5000,
+               description: "商品説明:洗練されたモダンデザインのハイバックチェアです",
                sale_status_id: 1,
+               product_status_id: 2,
+               regist_date:DateTime.parse('1993-02-24T12:30:45'),
+               user_id: 1,
+               delete_flag: false)
+
+Product.create!(product_name: "タンス",
+               category_id: 1,
+               price: 30000,
+               description: "商品説明:上質な空間を作り上げる、プレミアムな多段衣類収納チェストです",
+               sale_status_id: 2,
                product_status_id: 1,
                regist_date:DateTime.parse('1993-02-24T12:30:45'),
                user_id: 1,
                delete_flag: false)
 
-Product.create!(product_name: "テーブル",
+Product.create!(product_name: "ベッド",
                category_id: 1,
-               price: 10000,
-               description: "商品説明",
-               sale_status_id: 1,
-               product_status_id: 1,
+               price: 40000,
+               description: "商品説明:シンプルで使いやすいフランスベッドのマットレスベッドです",
+               sale_status_id: 2,
+               product_status_id: 2,
                regist_date:DateTime.parse('1993-02-24T12:30:45'),
                user_id: 1,
                delete_flag: false)
 
-Product.create!(product_name: "テーブル",
-               category_id: 1,
-               price: 10000,
-               description: "商品説明",
-               sale_status_id: 1,
-               product_status_id: 1,
-               regist_date:DateTime.parse('1993-02-24T12:30:45'),
-               user_id: 1,
-               delete_flag: false)
-
-Purchase.create!(purchase_price: 10000,
-                purchase_quantity: 1,
-                purchase_company: "株式会社aaa",
+Purchase.create!(purchase_price: 70000,
+                purchase_quantity: 5,
+                purchase_company: "株式会社NI",
                 order_date:DateTime.parse('1993-02-24T12:30:45'),
                 purchase_date:DateTime.parse('1993-02-24T12:30:45'),
                 product_id: 1)
 
-Purchase.create!(purchase_price: 10000,
-                purchase_quantity: 1,
-                purchase_company: "株式会社aaa",
+Purchase.create!(purchase_price: 30000,
+                purchase_quantity: 2,
+                purchase_company: "株式会社DI",
                 order_date:DateTime.parse('1993-02-24T12:30:45'),
                 purchase_date:DateTime.parse('1993-02-24T12:30:45'),
-                product_id: 1)
+                product_id: 2)
