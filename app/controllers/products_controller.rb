db6d5f0d21f7ea35449940fd612ebc1c68098aa1
Yokoyama-kouki <0k7u1k8@gmail.com>
2021-11-11 01:51:57 +0900

    コントローラー修正

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,9 +1,6 @@
 class ProductsController < ApplicationController
   def show
     @product = Product.find_by(id: params[:id])
-    if @product.blank?
-      redirect_to products_show_not_found_url
-    end
   end
 
 end
