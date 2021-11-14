9094b8c78611283e21e516edb2df4a57d7709ceb
Taira <89443713+hhqbjtaira@users.noreply.github.com>
2021-11-14 23:47:23 +0900

    users_controller.rb に update アクションを記載

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,4 +6,21 @@ class UsersController < ApplicationController
   def edit
     @user = User.find_by(id: params[:id])
   end
+
+  def update
+    @user = User.find_by(id: params[:id]).update(user_params)
+  end
+
+  private
+  def user_params
+    paramas.requie(:user).permit(:last_name,
+                                :first_name,
+                                :zipcode,
+                                :prefecture,
+                                :municipality,
+                                :address,
+                                :apartments,
+                                :phone_number,
+                                :email )
+  end
 end
