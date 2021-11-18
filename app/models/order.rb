a1383b57766e8862e489432ef0d78a3220c6c459
Yokoyama-kouki <0k7u1k8@gmail.com>
2021-11-19 02:48:00 +0900

    メソッド名修正

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,7 +2,7 @@ class Order < ApplicationRecord
   belongs_to :user
   has_many :order_details, dependent: :destroy
 
-  def order_preparing
+  def order_preparing?
     order_details.any? { |order_detail| order_detail.shipment_status.shipment_status_name == "準備中" }
   end
 
