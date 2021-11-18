50df93722f8e2a631511f5362ef9de69b07534f5
Yokoyama-kouki <0k7u1k8@gmail.com>
2021-11-19 05:35:03 +0900

    ユーザー登録関連アクション

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,37 @@ class UsersController < ApplicationController
   def show
     @user = User.find_by(id: params[:id])
   end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "ユーザーを登録しました。こちらからログインしてください。"
+      redirect_to login_path
+    else
+      flash.now[:alert] = "登録に失敗しました"
+      render "new"
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(
+      :last_name,
+      :first_name,
+      :zipcode,
+      :prefecture,
+      :municipality,
+      :address,
+      :apartments,
+      :email,
+      :phone_number,
+      :password,
+      :password_confirmation,
+    )
+  end
 end
