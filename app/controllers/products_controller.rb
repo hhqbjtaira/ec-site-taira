00dbb02b1bf923196f745fc3384b99095204a046
Yokoyama-kouki <0k7u1k8@gmail.com>
2021-11-20 19:44:57 +0900

    indexアクション定義

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,4 +2,9 @@ class ProductsController < ApplicationController
   def show
     @product = Product.find_by(id: params[:id])
   end
+
+  def index
+    @products = Product.all
+    @categories = Category.all
+  end
 end
