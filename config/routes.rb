7dbd2c6ac56f85817d2c9ff0c4b1f182952a7957
hhqbjtaira <hhqbjjob@gmail.com>
2021-11-22 03:11:00 +0900

    routes.rb にログイン、ログアウトを追加

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   get '/sign_up', to: 'users#new'
   post '/sign_up', to: 'users#create'
   get 'login', to:'sessions#new', as: 'login'
+  post    'login',   to: 'users#show', as: 'login'
+  delete  'logout',  to: 'sessions#new', as: 'logout'
   resources :orders
   resources :users
   resources :products
