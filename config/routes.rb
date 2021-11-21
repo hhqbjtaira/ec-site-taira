b65031946d6441b06bce61036ad81cfac293149a
hhqbjtaira <hhqbjjob@gmail.com>
2021-11-22 01:14:47 +0900

    routes.rb 修正

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,6 @@ Rails.application.routes.draw do
   get '/sign_up', to: 'users#new'
   post '/sign_up', to: 'users#create'
   get 'login', to:'sessions#new', as: 'login'
-  patch 'user', to: 'users#update'
-  put 'user', to: 'users#update'
   resources :orders
   resources :users
   resources :products
