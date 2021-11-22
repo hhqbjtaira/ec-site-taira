bc5e1a4ace3da6501174305f07ceaaa23052e790
hhqbjtaira <hhqbjjob@gmail.com>
2021-11-23 01:41:20 +0900

    session_controller.rb 修正

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,16 +3,17 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by(id: params[:id])
-    if !user.nil? && user.authenticate(id: params[:id])
+    user = User.find_by(email: params[:session][:email])
+    if !user.nil? && user.authenticate(email: params[:session][:email])
       log_in user
       redirect_to user
-      session[:id] = user.id
+      session[:id] = user.id, notice: 'ログイン成功'
     else
-      flash.now[:danger] = 'Invalid email/password combination'
+      flash.now[:danger] = '登録に失敗しました'
       render 'new'
       session.delete(:id)
     end
+
   end
 
   def destroy
