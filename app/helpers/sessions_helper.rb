3811ba68ee37c88a2f5b8aeecd413eed0f655407
hhqbjtaira <hhqbjjob@gmail.com>
2021-11-23 01:41:55 +0900

    current_user 修正

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -12,15 +12,8 @@ module SessionsHelper
 
   # current_user を定義
   def current_user
-    if (user_id = session[:user_id])
-      @current_user ||= User.find_by(id: user_id)
-    elsif (user_id = cookies.signed[:user_id])
-      user = User.find_by(id: user_id)
-      if user && user.authenticated?(:remember, cookies[:remember_token])
-        log_in user
-        @current_user = user
-      end
-    end
+    User.find_by(id: session[:user_id])
+    @current_user = user
   end
 
   # current_userがログイン済みユーザーであればtrueを返す
