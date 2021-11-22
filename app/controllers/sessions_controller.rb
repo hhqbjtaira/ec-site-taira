ec9affa792f648ee8e55ad468ca19d4a4ad61284
hhqbjtaira <hhqbjjob@gmail.com>
2021-11-22 20:44:01 +0900

    sessions_controller に create を作成

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,21 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+    user = User.find_by(id: params[:id])
+    if !user.nil? && user.authenticate(id: params[:id])
+      log_in user
+      redirect_to user
+      session[:id] = user.id
+    else
+      flash.now[:danger] = 'Invalid email/password combination'
+      render 'new'
+      session.delete(:id)
+    end
+  end
+
+  def destroy
+  end
+
 end
