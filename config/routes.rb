e49445524bb2f9c3a55ff12761a9a3b074518fc0
hhqbjtaira <hhqbjjob@gmail.com>
2021-11-23 01:43:01 +0900

    routes.rb 修正

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   get '/sign_up', to: 'users#new'
   post '/sign_up', to: 'users#create'
   get 'login', to:'sessions#new', as: 'login'
-  post    'login',   to: 'users#show', as: 'login'
-  delete  'logout',  to: 'sessions#new', as: 'logout'
+  post    'login',   to: 'sessions#create'
+  delete  'logout',  to: 'sessions#destroy', as: 'logout'
   resources :orders
   resources :users
   resources :products
