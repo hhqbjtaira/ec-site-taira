c1cb4a0292f4a57f26b09644b91b2afdc4382379
Yokoyama-kouki <0k7u1k8@gmail.com>
2021-11-22 10:34:42 +0900

    rubocop

--- app/models/order.rb
+++ app/models/order.rb
@@ -3,7 +3,7 @@ class Order < ApplicationRecord
   has_many :order_details, dependent: :destroy
 
   def order_preparing?
-    order_details.any? { |order_detail| order_detail.shipment_status.shipment_status_name == "準備中" }
+    order_details.any? {|order_detail| order_detail.shipment_status.shipment_status_name == "準備中" }
   end
 
   def total_price
