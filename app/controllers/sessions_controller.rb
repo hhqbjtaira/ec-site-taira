889311be873369f881f560511321bed738bf0872
hhqbjtaira <hhqbjjob@gmail.com>
2021-11-23 13:38:09 +0900

    session_controller.rb 修正

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email])
     if !user.nil? && user.authenticate(email: params[:session][:email])
       log_in user
-      redirect_to user
+      redirect_to  products_path
     else
       flash.now[:danger] = '登録に失敗しました'
       render 'new'
