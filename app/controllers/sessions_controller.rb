a2c32b3284a6f2cb2715d011257398cd69c64689
hhqbjtaira <hhqbjjob@gmail.com>
2021-11-27 00:23:12 +0900

    sessions_controller 修正

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,20 +4,20 @@ class SessionsController < ApplicationController
 
   def create
     user = User.find_by(email: params[:session][:email])
-
     if !user.nil? && user.authenticate(params[:session][:password])
       login user
+      # 認証成功の場合
       redirect_to user
     else
-      flash.now[:danger] = '登録に失敗しました'
-      render 'new'
+      flash.now[:danger] = "登録に失敗しました"
+      # 認証失敗の場合
+      render "new"
     end
-
   end
 
   def destroy
-   logout if logged_in?
-   flash[:danger] = 'ログアウトしました'
-   redirect_to login_path
+    logout if logged_in?
+    flash[:danger] = "ログアウトしました"
+    redirect_to login_path
   end
 end
