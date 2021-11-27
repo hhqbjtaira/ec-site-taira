59dec4ea98cb2f754ebbada0c1f9ccc5b84f01aa
hhqbjtaira <hhqbjjob@gmail.com>
2021-11-27 17:05:34 +0900

    before_action に update を追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: [:edit, :show]
-  before_action :correct_user, only: [:edit, :show]
+  before_action :logged_in_user, only: [:edit, :show, :update]
+  before_action :correct_user, only: [:edit, :show, :update]
 
   def show
     @user = User.find_by(id: params[:id])
