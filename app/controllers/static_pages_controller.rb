fcc944da8da9e2c4527185e13e3065938ece028f
hhqbjtaira <hhqbjjob@gmail.com>
2021-12-03 23:03:12 +0900

    static_pages_controller.rb 修正

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,9 +1,4 @@
 class StaticPagesController < ApplicationController
   def home
-    @user = User.find_by(id: params[:id])
-  end
-
-  def home_after_login
-    @user = User.find_by(id: params[:id])
   end
 end
