5f163f7a623411b93c8149eee12cf81f820d2443
hhqbjtaira <hhqbjjob@gmail.com>
2021-12-04 19:31:52 +0900

    アローを修正

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,6 @@ Rails.application.routes.draw do
   post    'login',   to: 'sessions#create'
   delete  'logout',  to: 'sessions#destroy', as: 'logout'
   resources :orders
-  resources :users, :only => [:show, :edit, :update, :destroy]
+  resources :users, :only : [:show, :edit, :update, :destroy]
   resources :products
 end
