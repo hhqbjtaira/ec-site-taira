28a2fa7590b2203d404e181257bbfe1fb655cf11
Yokoyama-kouki <0k7u1k8@gmail.com>
2021-12-05 21:56:18 +0900

    orders#indexアクション作成

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,4 +2,8 @@ class OrdersController < ApplicationController
   def show
     @order = Order.find_by(id: params[:id])
   end
+
+  def index
+    @orders = current_user.orders.all.page(params[:page]).per(15)
+  end
 end
