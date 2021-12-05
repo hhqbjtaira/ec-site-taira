b33ec68edcb22378cedf247a38bd83b849bc1150
Yokoyama-kouki <0k7u1k8@gmail.com>
2021-12-05 18:00:13 +0900

    carts#showアクション作成

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,3 +1,18 @@
 class CartsController < ApplicationController
+  def show
+    @cart = []
+    session[:cart].each do |cart|
+      product = Product.find_by(id: cart["product_id"])
+      @cart.push({ 
+        product_id: product.id,
+        product_name: product.product_name,
+        category_name: product.category.category_name,
+        price: product.price,
+        quantity: cart["quantity"],
+        sub_total: product.price * cart["quantity"]
+        })
+        @total_price = @cart.sum { |cart| cart[:sub_total]}
+    end
+  end
 
 end
