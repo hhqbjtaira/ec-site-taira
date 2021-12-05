babc2f1b84aae8c60f87f8fafbb29f7269e1d1b2
Yokoyama-kouki <0k7u1k8@gmail.com>
2021-12-06 02:55:21 +0900

    orders#destroyアクション作成

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,4 +2,11 @@ class OrdersController < ApplicationController
   def show
     @order = Order.find_by(id: params[:id])
   end
+
+  def destroy
+    @order = Order.find_by(id: params[:id])
+    order_preparing = @order.order_details.select {|order_detail| order_detail.shipment_status_id == 1 }
+    @order.order_details.delete(order_preparing)
+    redirect_to order_path
+  end
 end
