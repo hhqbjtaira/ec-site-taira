d369ec9fc0ed211c87cc865cfb16265de8b1b165
Yokoyama-kouki <0k7u1k8@gmail.com>
2021-12-05 19:27:30 +0900

    rubocop

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -3,22 +3,22 @@ class CartsController < ApplicationController
     @cart = []
     session[:cart].each do |cart|
       product = Product.find_by(id: cart["product_id"])
-      @cart.push({ 
+      @cart.push({
         product_id: product.id,
         product_name: product.product_name,
         category_name: product.category.category_name,
         price: product.price,
         quantity: cart["quantity"],
-        sub_total: product.price * cart["quantity"]
-        })
-        @total_price = @cart.sum { |cart| cart[:sub_total]}
+        sub_total: product.price * cart["quantity"],
+      })
+      @total_price = @cart.sum {|cart| cart[:sub_total] }
     end
   end
 
   def add_cart
     if session[:cart].blank?
       session[:cart] = [{ product_id: params[:product_id], quantity: params[:quantity].to_i }]
-      return redirect_to('/carts/show')
+      return redirect_to("/carts/show")
     end
     duplication = session[:cart].select {|cart| cart["product_id"] == params[:product_id] }
     if duplication.present?
@@ -26,20 +26,20 @@ class CartsController < ApplicationController
     else
       session[:cart].push({ product_id: params[:product_id], quantity: params[:quantity].to_i })
     end
-    redirect_to('/carts/show')
+    redirect_to("/carts/show")
   end
 
   # カート内商品削除
   def destroy
     array = session[:cart].each_index.select {|i| session[:cart][i]["product_id"] == params[:product_id] }
     session[:cart].delete_at(array[0])
-    redirect_to('/carts/show')
+    redirect_to("/carts/show")
   end
 
   # カート内で数量変更
   def update
     array = session[:cart].each_index.select {|i| session[:cart][i]["product_id"] == params[:product_id] }
     session[:cart][array[0]]["quantity"] = params[:quantity].to_i
-    redirect_to('/carts/show')
+    redirect_to("/carts/show")
   end
 end
--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -5,14 +5,15 @@ class OrdersController < ApplicationController
 
   def create
     if session[:cart].blank?
-      return redirect_to('/carts/show')
+      return redirect_to("/carts/show")
     end
-    @order = current_user.orders.create(order_date: Time.now, order_number: rand(9999999999999999) )
+
+    @order = current_user.orders.create!(order_date: Time.now, order_number: rand(9_999_999_999_999_999))
     session[:cart].each do |cart|
       @order.order_details.create(
         product_id: cart["product_id"],
         shipment_status_id: 1,
-        order_detail_number: rand(9999999999999999),
+        order_detail_number: rand(9_999_999_999_999_999),
         order_quantity: cart["quantity"],
       )
     end
@@ -21,7 +22,6 @@ class OrdersController < ApplicationController
   end
 
   def purchase_completed
-    order_id = params[:order_id]
     @order = Order.find_by(id: params[:order_id])
   end
 end
