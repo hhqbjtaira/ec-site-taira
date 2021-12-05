9cdd10cfc347f9258be0defc3d22532263ec89fc
Yokoyama-kouki <0k7u1k8@gmail.com>
2021-12-05 18:01:04 +0900

    carts#add_cartアクション作成

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -15,4 +15,17 @@ class CartsController < ApplicationController
     end
   end
 
+  def add_cart
+    if session[:cart].blank?
+      session[:cart] = [{ product_id: params[:product_id], quantity: params[:quantity].to_i }]
+      return redirect_to('/carts/show')
+    end
+    duplication = session[:cart].select {|cart| cart["product_id"] == params[:product_id] }
+    if duplication.present?
+      duplication[0]["quantity"] += params[:quantity].to_i
+    else
+      session[:cart].push({ product_id: params[:product_id], quantity: params[:quantity].to_i })
+    end
+    redirect_to('/carts/show')
+  end
 end
