e900b74ca6c111a70376d32f4a378399516ccbcf
Yokoyama-kouki <0k7u1k8@gmail.com>
2021-12-08 20:10:05 +0900

    orders#show認可機能実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,18 +1,29 @@
 class OrdersController < ApplicationController
   before_action :logged_in_user
-  before_action :correct_user, only: [:show]
+  before_action :correct_order, only: [:show]
 
   def show
     @order = Order.find_by(id: params[:id])
   end
 
   def index
-  @orders = current_user.orders.page(params[:page]).per(15)
+    @orders = current_user.orders.page(params[:page]).per(15)
+  end
   
   def destroy
     @order = Order.find_by(id: params[:id])
     order_preparing = @order.order_details.select {|order_detail| order_detail.shipment_status_id == 1 }
     @order.order_details.delete(order_preparing)
     redirect_to order_path
+  end
+
+  private
+
+  def correct_order
+    order = Order.find_by(id: params[:id])
+    if current_user.orders.none? {|current_order| current_order == order }
+      flash[:danger] = "他人の情報にアクセスすることはできません。"
+      redirect_to root_path
+    end
   end
 end
