871ab0e26bd2360fd94d3be1a843190afd5d156d
Yokoyama-kouki <0k7u1k8@gmail.com>
2021-12-16 00:46:22 +0900

    users.rb作成

---
+++ spec/factories/users.rb
@@ -0,0 +1,16 @@
+FactoryBot.define do
+  factory :user do
+    password { "qwerty" }
+    last_name { "user0" }
+    first_name { "user1" }
+    zipcode { "731-3191" }
+    prefecture { "広島県" }
+    municipality { "広島市安佐南区" }
+    address { "大塚東" }
+    apartments { "1-1-1" }
+    sequence(:email) { |n| "tester#{n}@example.com" }
+    phone_number { "0821230001" }
+    company_name { "株式会社aaa" }
+    association :user_classification
+  end
+end
