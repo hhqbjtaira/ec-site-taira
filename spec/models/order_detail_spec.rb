c0250437b69107e5c06664840a2dd85f6efd83b5
Yokoyama-kouki <0k7u1k8@gmail.com>
2021-12-16 18:47:38 +0900

    メソッドテスト修正

--- spec/models/order_detail_spec.rb
+++ spec/models/order_detail_spec.rb
@@ -1,12 +1,20 @@
 require "rails_helper"
 
 RSpec.describe OrderDetail, type: :model do
-  it "商品詳細で個数と商品の値段の小計をテスト" do
-    order_detail = create(:order_detail)
-    product = order_detail.product
-    product.price = 500
-    order_detail.order_quantity = 2
-    expect(order_detail.subtotal_price).to eq 1000
+  describe "インスタンスメソッドのテスト" do
+    before do
+      @order_detail = create(:order_detail)
+      product = @order_detail.product
+      product.price = 500
+      @order_detail.order_quantity = 2
+    end
+
+    context "商品詳細で個数と商品の値段の小計" do
+      it "値段×個数となっている" do
+        order_detail = @order_detail
+        expect(order_detail.subtotal_price).to eq 1000
+      end
+    end
   end
 
   describe "アソシエーションのテスト" do
