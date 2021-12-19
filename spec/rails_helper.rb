cc396545421c40160787e107c8ec8868cc0d0eb2
Yokoyama-kouki <0k7u1k8@gmail.com>
2021-12-19 20:04:30 +0900

    sessionを使うための準備

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -63,4 +63,20 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+
+  # テストケース共通の事前処理
+  config.before(:each) do
+
+    # let(:rspec_session) で指定された値を セッションの初期値とします
+    session = defined?(rspec_session) ? rspec_session : {}
+
+    # destroyメソッドを実行してもエラーにならないようにします（必要であれば）
+    session.class_eval { def destroy; nil; end } 
+
+    # 実行後のセッションを取得できるようにする
+    config.add_setting(:session, :default => session)
+
+    # sessionメソッドを上書き
+    allow_any_instance_of(ActionDispatch::Request).to receive(:session).and_return(session)
+  end
 end
