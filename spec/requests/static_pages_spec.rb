05e36336a1245952c884c5501f15f18d2e6fa7e8
hhqbjtaira <hhqbjjob@gmail.com>
2021-12-22 00:10:11 +0900

    フォルダ名を requests に変更

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,8 +1,8 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe "StaticPages", type: :request do
   describe "GET /static_pages/home" do
-    it 'Top画面の表示に成功すること' do
+    it "Top画面の表示に成功すること" do
       get root_path
       expect(response).to have_http_status(200)
     end
