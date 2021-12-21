83fa56d6ebf357437cf94dc96723ffa623b5f83e
Yokoyama-kouki <0k7u1k8@gmail.com>
2021-12-22 07:24:39 +0900

    orders#createテスト

--- spec/requests/orders_spec.rb
+++ spec/requests/orders_spec.rb
@@ -19,33 +19,68 @@ RSpec.describe "Orders", type: :request do
         end
       end
 
+      describe "POST /orders/create" do
+        
+        let(:order_detail) { create(:order_detail, order_id: order.id, product_id: RSpec.configuration.session[:cart][0]["product_id"], shipment_status_id: shipment_status.id, order_quantity: RSpec.configuration.session[:cart][0]["quantity"]) }
+        let(:product) { create(:product) }
+        let(:shipment_status) { create(:shipment_status)}
+        let(:rspec_session) { { cart: product, user_id: user } }
 
-    describe "POST /orders/create" do
-      let(:order_detail) { create(:order_detail, order_id: order.id, product_id: product.id) }
-      let(:product) { create(:product) }
-      let(:rspec_session) { { cart: product } }
-      context "カート内が空の場合" do
-        before do
-          RSpec.configuration.session[:cart] = []
+        context "カート内が空の場合" do
+          before do
+            RSpec.configuration.session[:cart] = []
+          end
+
+          it "カート画面に遷移" do
+            order_detail = nil
+            post orders_path
+            expect(response).to redirect_to carts_show_path
+          end
         end
 
-        it "期待した結果が返ってくる" do
-          order_detail = nil
-          post '/orders'
-          expect(response).to redirect_to("http://www.example.com/carts/show")
+        context "カート内に商品がある場合" do
+          before do
+            RSpec.configuration.session[:cart] = [
+              "product_id" => product.id,
+              "quantity" => 3
+            ]
+          end
+          it "ユーザーに紐づくオーダーが作られる" do
+            expect{
+              post orders_path
+            }.to change(Order, :count).by(1)
+          end
+
+          context "orders#create内でオーダーの情報が必要なテスト" do
+            let!(:order) { create(:order, user_id: user.id, order_date: Time.current, order_number: rand(9_999_999_999_999_999)) }
+
+            it "注文詳細が作られる" do
+              expect{
+                post orders_path
+                RSpec.configuration.session[:cart] = [
+                  "product_id" => product.id,
+                  "quantity" => 3
+                ]
+                order_detail
+              }.to change(OrderDetail, :count).by(1)
+            end
+
+            it "カート内が空になっている" do
+              post orders_path
+              expect(RSpec.configuration.session[:cart]).to eq []
+            end
+          end
+
+          it "購入完了画面に遷移" do
+            post orders_path
+            order = Order.last
+            expect(response).to redirect_to purchase_completed_path(order_id: order.id)
+          end
         end
       end
 
-      context "カート内に商品がある場合" do
-        before do
-          @current_user = user
-          order
         end
 
-        it "ユーザーに紐づくオーダーが作られる" do
-          current_user = @current_user
-          post '/orders'
-          expect(response).to have_http_status(200)
         end
       end
     end
