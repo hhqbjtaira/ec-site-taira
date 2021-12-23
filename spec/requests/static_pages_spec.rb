2307050aebb792850c783500d7bcdf8b4e10e672
hhqbjtaira <hhqbjjob@gmail.com>
2021-12-23 17:35:27 +0900

    rubocop エラーの対処

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe "StaticPages", type: :request do
   describe "GET /static_pages/home" do
     it "Top画面の表示に成功すること" do
       get root_path
-      expect(response).to have_http_status(200)
+      expect(response).to have_http_status(:ok)
     end
   end
 end
