0b17352c61c9f2bb19743a1933852abc093dab2a
hhqbjtaira <hhqbjjob@gmail.com>
2021-12-25 14:56:46 +0900

    不要なコメントアウトの削除

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -1,54 +1,16 @@
 class ImageUploader < CarrierWave::Uploader::Base
-  # Include RMagick or MiniMagick support:
-  # include CarrierWave::RMagick
-  # [400, 200]は例なので画像のサイズは後で指定
   include CarrierWave::MiniMagick
   version :thumb100 do
     process resize_to_fit: [100, 100]
   end
-
-  # Choose what kind of storage to use for this uploader:
   storage :file
+  # S3を設定したら以下に変更
   # storage :fog
 
-  # Override the directory where uploaded files will be stored.
-  # This is a sensible default for uploaders that are meant to be mounted:
   # アップロードしたファイルの保存先を指定する。
   def store_dir
     "uploads/#{user.class.to_s.underscore}/#{image}/#{user.id}"
   end
-
-  # Provide a default URL as a default if there hasn't been a file uploaded:
-  # def default_url(*args)
-  #   # For Rails 3.1+ asset pipeline compatibility:
-  #   # ActionController::Base.helpers.asset_path("fallback/" + [version_name, "default.png"].compact.join('_'))
-  #
-  #   "/images/fallback/" + [version_name, "default.png"].compact.join('_')
-  # end
-
-  # Process files as they are uploaded:
-  # process scale: [200, 300]
-  #
-  # def scale(width, height)
-  #   # do something
-  # end
-
-  # Create different versions of your uploaded files:
-  # version :thumb do
-  #   process resize_to_fit: [50, 50]
-  # end
-
-  # Add an allowlist of extensions which are allowed to be uploaded.
-  # For images you might use something like this:
-  # def extension_allowlist
-  #   %w(jpg jpeg gif png)
-  # end
-
-  # Override the filename of the uploaded files:
-  # Avoid using model.id or version_name here, see uploader/store.rb for details.
-  # def filename
-  #   "something.jpg" if original_filename
-  # end
 
   # 画像がアップロードされてない時に、'sample.jpg'を表示する。
   def default_url
