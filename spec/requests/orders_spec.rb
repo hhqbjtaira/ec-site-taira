eae4087a5ed5f4053792404574cab8f3c0020989
Yokoyama-kouki <0k7u1k8@gmail.com>
2021-12-26 22:24:35 +0900

    orders#createテスト修正

--- spec/requests/orders_spec.rb
+++ spec/requests/orders_spec.rb
@@ -23,17 +23,8 @@ RSpec.describe "Orders", type: :request do
       end
 
       describe "POST /orders/create" do
-        let(:order_detail) {
-          create(
-            :order_detail,
-            order_id: order.id,
-            product_id: RSpec.configuration.session[:cart][0]["product_id"],
-            shipment_status_id: shipment_status.id,
-            order_quantity: RSpec.configuration.session[:cart][0]["quantity"],
-          )
-        }
         let(:product) { create(:product) }
-        let(:shipment_status) { create(:shipment_status) }
+        let(:shipment_status) { create(:shipment_status, id: 1) }
         let(:rspec_session) { { cart: product, user_id: user } }
 
         context "カート内が空の場合" do
@@ -53,32 +44,18 @@ RSpec.describe "Orders", type: :request do
               "product_id" => product.id,
               "quantity" => 3,
             ]
+            shipment_status
           end
 
-          it "ユーザーに紐づくオーダーが作られる" do
+          it "注文および注文詳細が作られる" do
             expect {
               post orders_path
-            }.to change { Order.count }.by(1)
+            }.to change { Order.count }.by(1).and change { OrderDetail.count }.by(1)
           end
 
-          context "orders#create内でオーダーの情報が必要なテスト" do # rubocop:disable RSpec/MultipleMemoizedHelpers
-            let(:order) { create(:order, user_id: user.id, order_date: Time.current, order_number: rand(9_999_999_999_999_999)) }
-
-            it "注文詳細が作られる" do
-              expect {
-                post orders_path
-                RSpec.configuration.session[:cart] = [
-                  "product_id" => product.id,
-                  "quantity" => 3,
-                ]
-                order_detail
-              }.to change { OrderDetail.count }.by(1)
-            end
-
-            it "カート内が空になっている" do
-              post orders_path
-              expect(RSpec.configuration.session[:cart]).to eq []
-            end
+          it "カート内が空になっている" do
+            post orders_path
+            expect(RSpec.configuration.session[:cart]).to eq []
           end
 
           it "購入完了画面に遷移" do
