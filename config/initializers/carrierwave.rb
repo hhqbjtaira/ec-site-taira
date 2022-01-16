cbef7e83a9e38fe269583e1f53865ca376ce23f6
hhqbjtaira <hhqbjjob@gmail.com>
2022-01-17 00:26:41 +0900

    config.fog_public = false

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -5,6 +5,7 @@ CarrierWave.configure do |config|
   config.storage :fog
   config.fog_provider = 'fog/aws'
   config.fog_directory  = 'testbacket1'
+  config.fog_public = false
   config.fog_credentials = {
     provider: 'AWS',
     aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
