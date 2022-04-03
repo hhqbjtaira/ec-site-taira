d5e282e2f4df7dd455e8bc2fe08f3a95266e723b
Taira <89443713+hhqbjtaira@users.noreply.github.com>
2022-04-04 06:51:54 +0900

    s3バケットの変更

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -4,7 +4,7 @@ CarrierWave.configure do |config|
   if Rails.env.production?
   config.storage :fog
   config.fog_provider = 'fog/aws'
-  config.fog_directory  = 'testbacket1'
+  config.fog_directory  = ENV['S3_BUCKET']
   config.fog_public = false
   config.fog_credentials = {
     provider: 'AWS',
